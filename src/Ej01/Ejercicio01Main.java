58d3310c913f50550a61cfc531c6378852e307fe
Marc Pellicer <marcpelli3@gmail.com>
2023-10-10 16:47:39 +0200

    Ejercicio 01 - Unidad 10

---
+++ src/Ej01/Ejercicio01Main.java
@@ -0,0 +1,11 @@
+package Ej01;
+
+public class Ejercicio01Main {
+
+	public static void main(String[] args) {
+		
+		Juego partida = new Juego();
+		partida.jugar();
+
+	}	 	
+}
---
+++ src/Ej01/Juego.java
@@ -0,0 +1,54 @@
+package Ej01;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class Juego {
+
+	public static void jugar() {
+		
+		Random rnd = new Random();
+		int numero = rnd.nextInt(500) + 1;
+		System.out.println(numero);
+		int intento = 0;
+		
+		while (true) {
+			
+			try {
+				
+				String usuario = JOptionPane.showInputDialog(null, "Adivina el numero entre 1 y 500:");
+				int numUsuario = Integer.parseInt(usuario);
+				
+				if (numUsuario < 1 || numUsuario > 500) {
+					
+					JOptionPane.showMessageDialog(null, "Tiene que ser entre  1 y 500");
+					intento++;
+					
+				} else if (numUsuario < numero) {
+					
+					JOptionPane.showMessageDialog(null, "Tiene que ser mayor al numero introducido");
+					intento++;
+
+				} else if (numUsuario > numero) {
+					
+					JOptionPane.showMessageDialog(null, "Tiene que ser menor al numero introducido");
+					intento++;
+
+				} else {
+					
+					intento++;
+					JOptionPane.showMessageDialog(null, "Wow! Has adivinado el numero: " + numero + " a la " + intento + " vez");
+					
+					break;
+				}
+			} catch (InputMismatchException e){
+				
+				JOptionPane.showMessageDialog(null, "Ingresa un numero valido");
+				intento++;
+			}
+		
+		}
+	}
+	
+}
